cacf563120e01be55a9fba1f18d21985505e109f
Elio Struyf <info@estruyf.be>
2020-02-21 14:31:19 +0100

    Updated config

---
+++ script/generate-config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const config = fs.readFileSync(path.join(__dirname, '../cypress.sample.json'), { encoding: "utf8" });
+const configJson = JSON.parse(config);
+configJson.env = {
+  username: process.env.USERNAME,
+  username: process.env.PASSWORD
+};
+
+if (process.env.VERBOSE) {
+  console.log(configJson);
+}
+
+fs.writeFileSync(path.join(__dirname, `../cypress${process.env.CI ? "" : ".test"}.json`), JSON.stringify(configJson, null, 2));
