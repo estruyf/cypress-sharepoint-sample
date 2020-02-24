35c0cbfe5f024707133b6f8f5b987d29d10c2580
Elio Struyf <info@estruyf.be>
2020-02-24 16:30:00 +0100

    Updated pipeline with stages

--- script/generate-config.js
+++ script/generate-config.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
+// Fetch the sample file
 const config = fs.readFileSync(path.join(__dirname, '../cypress.sample.json'), { encoding: "utf8" });
+
+// Adding the username and password from the variable group
 const configJson = JSON.parse(config);
 configJson.env = {
   username: process.env.USERNAME,
   password: process.env.PASSWORD
 };
 
+// Rename the junit file based on the browser used
 configJson.reporterOptions.mochaFile = `cypress/reports/junit.${process.env.BROWSER || "unknown"}-[hash].xml`;
 
+// Check if there is a project ID specified
+if (process.env.PROJECTID) {
+  configJson.projectId = process.env.PROJECTID;
+}
+
+// Logging the config
 if (process.env.VERBOSE) {
   console.log(configJson);
 }
